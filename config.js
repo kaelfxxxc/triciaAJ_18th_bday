fa612f637d6162d4ce13f5b605838f7c3badb3b0
kaelfxxxc <131323614+kaelfxxxc@users.noreply.github.com>
2023-04-27 22:37:23 +0800

    Add files via upload

---
+++ config.js
@@ -0,0 +1,43 @@
+// 霸都丶傲天 2019年10月10日 https://github.com/AJLoveChina/birthday
+var config = {
+    // 句子的长度可以任意， 你可以写十句话， 二十句话都可以
+    // 每句话尽量不要超过15个字,不然展示效果可能不太好
+    texts: [
+      
+        "from kael, to you MISS TriciaAJ",      //这里,每句话结尾的最后一个逗号必须是英文的哦!! 很重要哦!!
+        "I know some of my action,words that i say insult make you're night feels bad",  // 同上...
+        "Maybe, this is the last time or if lord help us to cross our path at future but big chances,its not gonna happen",
+        "Na sulte na naku ang ako ganahan i sulte nimo, kebaw naka ana.Wako ga mahay kay naa sab daghan nga need ma solve nga question.Mao nga wako ga duha2 i already accept wether its a yes or no pero mas more ang no",
+        "Im happy, not happy but supper happy that time na we talk through chat,katawa and that time i slowly and admit that i have feelings for you",
+        "i put it already sa akung mind nga,dili ko ma hegugma nag balik its just a bussiness kay i know it if ma inlove kos isa ka person. mo effort ko dayun mao nay pinaka hate naku nga bisan way sure. mo sugal ko dayun",
+        "Mao nay pinaka dili naku if ma inlove naku,set a side naku tanang bad go lang ko bisan way klaro if akung effort is worth it or not. atleast i show my real feelings and pure intention through action not only by words",
+        "And i think nga bisan d pani enough nga pasayluon ko nimo sa akung nabuhat, na sturya nga moy henungdan nga na insulto kas ako. Im really sorry. wako ga huna huna sa imong side if okay raba ka? or else. my expectation is wrong",
+        "You're part in my journey trish, siguro this time were not meant to be nga ang duha ka path is ma isa.Kasabot rko sa side nimo. This is the only way para maka pasaylo ko nimo even bati ning style nga ako ge buhat.",
+        "Also salamat pud ko sa mga supportive nimo nga close friend kebaw nakag knsa. Salamat sa time nga nakaila tamo. naka sturya, nagkauban sa isa ka place bisan d memorable but d tamo malimtan. GOOODLUCK SA IMONG JOURNEY TRISH,AS YOU SAID BEFORE NGA LIFE IS UNFAIR, YES ITS TRUE. but we need to continue what we've started and achieve goal make's your parent proud. ONCE AGAIN TRISH! HAPPY 18 BDAY ENJOY YOURE DAY, MORE BLESSINGS TO COME MORE CANDLES TO BLOW, KEEP SAFEE ALWAYS AND GOD PROTECT YOU ALWAYS, FAMILY, FRIENDS",
+        "KAEL~~",
+    ],
+    /**
+     * imgs 可以不填, 但是如果要填写的话必须遵循下面的格式
+     * "对应上面的文字, 要完全一样" : "图片地址, 可以把图片放在imgs文件夹中"
+     * 例如
+     * "心爱的小可爱": "./imgs/xiaokeai.jpg"
+     *
+     * 如果不要图片的话, 直接在每行开头写两个斜杠注释即可, 例如下面的 "今天是你的生日" 的图片就不会展示了:)
+     * Tip: 图片最好用正方形or接近正方形, 看起来效果更好
+     */
+    imgs: {
+        "心爱的小可爱": "./imgs/xiaokeai.png",
+        // "今天是你的生日": "./imgs/birthday.jpg",
+    },
+    // 按钮文字描述, 以下是默认的按钮文字，英文的，您可以改成你喜欢的文字
+    desc: {
+        turn_on: "Lights on, lights off?",
+        play: "Play music",
+        bannar_coming: "HAPPY LEGALITY, HAPPY 18TH BIRTHDAY!",
+        balloons_flying: "syempre naa me ballon, special baya ka",
+        cake_fadein: "Ganahan kag cake? tap kay mag order ta",
+        light_candle: "EYYYY!!! i blow dayun ang candle and make a wish",
+        wish_message: "unsa may wish nimo?",
+        story: "Syempre, naa me message para nimo",
+    }
+};
